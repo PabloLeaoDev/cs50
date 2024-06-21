0f4ea10f7003ef91f8fe7e32d90583014c7a6057
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-06-21 12:04:34 -0300

    directory changed

--- c/your-name.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main (void)
-{
-    string name = get_string("What's your name? ");
-    printf("Hey %s\n", name);
-}
-// esse código não funciona fora do ambiente do CS50
